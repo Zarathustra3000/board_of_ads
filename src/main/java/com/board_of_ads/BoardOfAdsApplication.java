484efa7e10dca142c4022bf2fce4e8c35536c2a7
Niyazbek <kunaev993@gmail.com>
2020-09-18 19:44:47 +0600

    Added email service

--- src/main/java/com/board_of_ads/BoardOfAdsApplication.java
+++ src/main/java/com/board_of_ads/BoardOfAdsApplication.java
@@ -1,13 +1,25 @@
 package com.board_of_ads;
 
+import com.board_of_ads.service.SendEmailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class BoardOfAdsApplication {
 
+    @Autowired
+    private SendEmailService sendEmailService;
+
     public static void main(String[] args) {
         SpringApplication.run(BoardOfAdsApplication.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void triggerWhenStarts(){
+        sendEmailService.sendEmail("ggg@gmail.com", "Hi there", "Test");
     }
 
 }
