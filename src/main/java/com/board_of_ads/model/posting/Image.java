94daff5a02f71fa255683fdaa54e7fc89401e3fd
SidAilen <SidAilen.me@gmail.com>
2020-09-18 03:00:28 +0300

    Done CarPosting class

--- src/main/java/com/board_of_ads/model/posting/Image.java
+++ src/main/java/com/board_of_ads/model/posting/Image.java
@@ -1,7 +1,41 @@
 package com.board_of_ads.model.posting;
 
+import com.board_of_ads.model.User;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "images_link_db")
 public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
     private String pathURL;
+
+    public Image() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPathURL() {
+        return pathURL;
+    }
+
+    public void setPathURL(String pathURL) {
+        this.pathURL = pathURL;
+    }
 }
