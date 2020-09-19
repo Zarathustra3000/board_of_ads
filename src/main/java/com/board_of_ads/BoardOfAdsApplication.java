c904649f8dbc43195a22674902d40f4b034e8cde
Ниязбек Аманжолов <47739624+NiyazbekAmanzholov@users.noreply.github.com>
2020-09-19 20:40:40 +0600

    Update BoardOfAdsApplication.java

--- src/main/java/com/board_of_ads/BoardOfAdsApplication.java
+++ src/main/java/com/board_of_ads/BoardOfAdsApplication.java
@@ -1,6 +1,5 @@
 package com.board_of_ads;
 
-import com.board_of_ads.service.SendEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
