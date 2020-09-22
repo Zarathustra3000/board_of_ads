6c39d5aad9716486c97e07e1f148358f5d2015a0
Dmitriy <fed3375546@yandex.ru>
2020-09-22 22:43:38 +0300

    Autowired UserService & RoleService to DataInitializer

--- src/main/java/com/board_of_ads/configs/DataInitializer.java
+++ src/main/java/com/board_of_ads/configs/DataInitializer.java
@@ -1,5 +1,7 @@
 package com.board_of_ads.configs;
 
+import com.board_of_ads.service.interfaces.RoleService;
+import com.board_of_ads.service.interfaces.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -8,6 +10,9 @@ import javax.annotation.PostConstruct;
 @Component
 @AllArgsConstructor
 public class DataInitializer {
+
+    private final UserService userService;
+    private final RoleService roleService;
 
     @PostConstruct
     private void init() {
