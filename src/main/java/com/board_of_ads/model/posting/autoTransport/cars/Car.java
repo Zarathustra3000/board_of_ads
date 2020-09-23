c0fd80e0141dcb18c717b0e476649d3ab5440380
SDA-mind <dima-sherbakov@mail.ru>
2020-09-23 13:44:06 +0300

    refactor

--- src/main/java/com/board_of_ads/model/posting/autoTransport/cars/Car.java
+++ src/main/java/com/board_of_ads/model/posting/autoTransport/cars/Car.java
@@ -1,60 +1,82 @@
-package com.board_of_ads.model.posting.autoTransport;
+package com.board_of_ads.model.posting.autoTransport.cars;
 
+import com.board_of_ads.model.posting.autoTransport.AutoTransport;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.Set;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
-@MappedSuperclass
+@Inheritance(strategy = InheritanceType.JOINED)
 @Entity
-public class NewCarPosting extends AutoTransportPosting {
+@Table(name = "cars")
+public class Car extends AutoTransport {
 
+    @Column
     private String color;
 
+    @Column
     private String VIN;
 
+    @Column
     private String brand;
 
+    @Column
     private String model;
 
+    @Column
     private Short year;
 
+    @Column
     private String carBody;
 
+    @Column
     private Byte numberOfDoors;
 
+    @Column
     private String generation;
 
+    @Column
     private String typeOfEngine;
 
+    @Column
     private String wheelDrive;
 
+    @Column
     private String transmission;
 
+    @Column
     private String modification;
 
+    @Column
     private String configuration;
 
+    @Column
     private String wheelSide;
 
+    @Column
     private boolean hasServiceBook;
 
+    @Column
     private boolean hasDealerService;
 
+    @Column
     private boolean UnderWarranty;
 
-    @OneToMany(mappedBy = "newCarPosting",
+    @OneToMany(mappedBy = "car",
             fetch = FetchType.EAGER,
             cascade = CascadeType.MERGE)
     private Set<Option> additionalOptions;
