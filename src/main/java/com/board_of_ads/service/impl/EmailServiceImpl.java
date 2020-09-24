22438865262cf54f5f12a0c1d495d6564fbae5d0
Niyazbek <kunaev993@gmail.com>
2020-09-24 21:37:08 +0600

    Отрефакторил код

--- src/main/java/com/board_of_ads/service/impl/EmailServiceImpl.java
+++ src/main/java/com/board_of_ads/service/impl/EmailServiceImpl.java
@@ -1,18 +1,20 @@
 package com.board_of_ads.service.impl;
 
+import com.board_of_ads.service.interfaces.EmailService;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SendEmailService {
+public class EmailServiceImpl implements EmailService {
 
     private final JavaMailSender javaMailSender;
 
-    public SendEmailService(JavaMailSender javaMailSender) {
+    public EmailServiceImpl(JavaMailSender javaMailSender) {
         this.javaMailSender = javaMailSender;
     }
 
+    @Override
     public void sendEmail(String to, String body, String topic) {
         System.out.println("sending email");
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
