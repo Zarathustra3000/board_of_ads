7294ff164c24d8e491eddba2a9ebedb87d3d68f0
SidAilenMe <sidailen.me@gmail.com>
2020-09-25 18:30:04 +0300

    done

--- src/main/java/com/board_of_ads/controllers/simple/MainPageController.java
+++ src/main/java/com/board_of_ads/controllers/simple/MainPageController.java
@@ -1,49 +1,33 @@
 package com.board_of_ads.controllers.simple;
 
-import com.board_of_ads.configs.auth.Auth;
-import com.board_of_ads.configs.auth.AuthVK;
-import com.board_of_ads.model.User;
-import com.board_of_ads.service.interfaces.UserService;
+import com.board_of_ads.service.interfaces.AuthService;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.time.LocalDateTime;
-import java.util.Map;
-
 @Controller
 @AllArgsConstructor
 public class MainPageController {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-    private final UserService userService;
-    private final UserDetailsService userDetailsService;
-    private final Auth auth;
-    private final AuthVK authVK;
+    private final AuthService authService;
 
     @GetMapping("/")
     public String getMainPage() {
         return "main-page";
     }
 
-
     @GetMapping("/vk_auth")
     public String vkAuth(@RequestParam(value = "code") String code, Model model) {
-        return vkAuthService.vkAuth(code);
+        return authService.vkAuth(code);
     }
 
     @GetMapping("/yandex_auth")
     public String yandexAuth(@RequestParam(value = "code") String code, Model model) {
-        String requestBody = authYandex.getRequestBody(code);
-        String token = authYandex.getToken(requestBody);
-        Map<String, String> userData = authYandex.getUserData(token);
-        User user = auth.init(userData);
-        auth.login(user);
-        return "redirect:/";
+        return authService.yandexAuth(code);
     }
 
     /** todo delete
