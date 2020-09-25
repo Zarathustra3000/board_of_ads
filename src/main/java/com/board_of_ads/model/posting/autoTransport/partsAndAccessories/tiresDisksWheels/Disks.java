4ce9acb8fc4559bf6d918bb8b27732cd5a591ebe
SDA-mind <dima-sherbakov@mail.ru>
2020-09-25 09:32:35 +0300

    refactor imports

--- src/main/java/com/board_of_ads/model/posting/autoTransport/partsAndAccessories/tiresDisksWheels/Disks.java
+++ src/main/java/com/board_of_ads/model/posting/autoTransport/partsAndAccessories/tiresDisksWheels/Disks.java
@@ -1,9 +1,7 @@
 package com.board_of_ads.model.posting.autoTransport.partsAndAccessories.tiresDisksWheels;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
