2dac22d4fd35e44353170936059bb55df03cf44a
zuryanov <zuryanovalexei@gmail.com>
2020-09-28 13:09:54 +0500

    removed unused imports

--- src/main/java/com/board_of_ads/controllers/simple/RestController.java
+++ src/main/java/com/board_of_ads/controllers/simple/RestController.java
@@ -7,9 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api")
