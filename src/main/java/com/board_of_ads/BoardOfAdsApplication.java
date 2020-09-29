5eef5b6dedb424951a020d9ac0120d4c2e0ac431
Sanaris <karpovvasilii@gmail.com>
2020-09-29 18:14:48 +0300

    Added logger configuration to app.yml and added logger to start point of application

--- src/main/java/com/board_of_ads/BoardOfAdsApplication.java
+++ src/main/java/com/board_of_ads/BoardOfAdsApplication.java
@@ -1,13 +1,26 @@
 package com.board_of_ads;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import javax.annotation.PreDestroy;
 
 @SpringBootApplication
 public class BoardOfAdsApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(BoardOfAdsApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(BoardOfAdsApplication.class, args);
     }
-  
+
+    @EventListener(ApplicationReadyEvent.class)
+    private void onStart() {
+        logger.info("Successful application launch");
+    }
+
 }
