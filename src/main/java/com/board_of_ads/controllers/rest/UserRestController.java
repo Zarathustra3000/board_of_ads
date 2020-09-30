220c655eeb9b1b1da8d369c1e43fcf1324ebb889
zuryanov <zuryanovalexei@gmail.com>
2020-09-30 19:51:21 +0500

    added returning response in restControllers

--- src/main/java/com/board_of_ads/controllers/rest/UserRestController.java
+++ src/main/java/com/board_of_ads/controllers/rest/UserRestController.java
@@ -1,16 +1,18 @@
 package com.board_of_ads.controllers.rest;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.security.Principal;
 
 @RestController
-@RequestMapping("/api/user/**")
+@RequestMapping("/api/user/")
 public class UserRestController {
 
     @GetMapping
-    public Principal getUser(Principal user) {
-        return user;
+    public ResponseEntity<Principal> getUser(Principal user) {
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 }
