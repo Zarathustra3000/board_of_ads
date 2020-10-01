962a9655d4d03d6dfcf0358320b4d74431f4312e
zuryanov <zuryanovalexei@gmail.com>
2020-10-01 16:30:29 +0500

    refactor to rest controllers

--- src/main/java/com/board_of_ads/controllers/rest/PostingRestController.java
+++ src/main/java/com/board_of_ads/controllers/rest/PostingRestController.java
@@ -3,8 +3,6 @@ package com.board_of_ads.controllers.rest;
 import com.board_of_ads.models.dto.PostingDto;
 import com.board_of_ads.service.interfaces.CityService;
 import com.board_of_ads.service.interfaces.PostingService;
-import com.board_of_ads.util.Error;
-import com.board_of_ads.util.ErrorResponse;
 import com.board_of_ads.util.Response;
 import com.board_of_ads.util.SuccessResponse;
 import lombok.AllArgsConstructor;
@@ -25,34 +23,16 @@ public class PostingRestController {
 
     @GetMapping
     public Response<List<PostingDto>> findAllPosts() {
-        List<PostingDto> posts;
-        try {
-            posts = postingService.getAllPostings();
-        } catch (Exception e) {
-            return new ErrorResponse<>(new Error(400, "No find posts"));
-        }
-        return new SuccessResponse<>(posts);
+        return new SuccessResponse<>(postingService.getAllPostings());
     }
 
     @GetMapping("/city/{name}")
     public Response<List<PostingDto>> findPostingsByCityName(@PathVariable String name) {
-        List<PostingDto> posts;
-        try {
-            posts = postingService.getPostingByCity(cityService.findCityByName(name).get());
-        } catch (Exception e) {
-            return new ErrorResponse<>(new Error(400, "No find posts"));
-        }
-        return new SuccessResponse<>(posts);
+        return new SuccessResponse<>(postingService.getPostingByCity(cityService.findCityByName(name).get()));
     }
 
     @GetMapping("/region/{name}")
     public Response<List<PostingDto>> findPostingsByRegionName(@PathVariable String name) {
-        List<PostingDto> posts;
-        try {
-            posts = postingService.getPostingByFullRegionName(name);
-        } catch (Exception e) {
-            return new ErrorResponse<>(new Error(400, "No find posts"));
-        }
-        return new SuccessResponse<>(posts);
+        return new SuccessResponse<>(postingService.getPostingByFullRegionName(name));
     }
 }
