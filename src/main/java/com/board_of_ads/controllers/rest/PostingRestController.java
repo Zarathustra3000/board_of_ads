8165fe9a8579b00ef1e870121dd5bbb278d7ca92
zuryanov <zuryanovalexei@gmail.com>
2020-10-01 15:59:35 +0500

    refactor to rest controllers

--- src/main/java/com/board_of_ads/controllers/rest/PostingRestController.java
+++ src/main/java/com/board_of_ads/controllers/rest/PostingRestController.java
@@ -3,9 +3,11 @@ package com.board_of_ads.controllers.rest;
 import com.board_of_ads.models.dto.PostingDto;
 import com.board_of_ads.service.interfaces.CityService;
 import com.board_of_ads.service.interfaces.PostingService;
+import com.board_of_ads.util.Error;
+import com.board_of_ads.util.ErrorResponse;
+import com.board_of_ads.util.Response;
+import com.board_of_ads.util.SuccessResponse;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,17 +24,35 @@ public class PostingRestController {
     private final PostingService postingService;
 
     @GetMapping
-    public ResponseEntity<List<PostingDto>> findAllPosts() {
-        return new ResponseEntity<>(postingService.getAllPostings(), HttpStatus.OK);
+    public Response<List<PostingDto>> findAllPosts() {
+        List<PostingDto> posts;
+        try {
+            posts = postingService.getAllPostings();
+        } catch (Exception e) {
+            return new ErrorResponse<>(new Error(400, "No find posts"));
+        }
+        return new SuccessResponse<>(posts);
     }
 
     @GetMapping("/city/{name}")
-    public ResponseEntity<List<PostingDto>> findPostingsByCityName(@PathVariable String name) {
-        return new ResponseEntity<>(postingService.getPostingByCity(cityService.findCityByName(name).get()), HttpStatus.OK);
+    public Response<List<PostingDto>> findPostingsByCityName(@PathVariable String name) {
+        List<PostingDto> posts;
+        try {
+            posts = postingService.getPostingByCity(cityService.findCityByName(name).get());
+        } catch (Exception e) {
+            return new ErrorResponse<>(new Error(400, "No find posts"));
+        }
+        return new SuccessResponse<>(posts);
     }
 
     @GetMapping("/region/{name}")
-    public ResponseEntity<List<PostingDto>> findPostingsByRegionName(@PathVariable String name) {
-        return new ResponseEntity<>(postingService.getPostingByFullRegionName(name), HttpStatus.OK);
+    public Response<List<PostingDto>> findPostingsByRegionName(@PathVariable String name) {
+        List<PostingDto> posts;
+        try {
+            posts = postingService.getPostingByFullRegionName(name);
+        } catch (Exception e) {
+            return new ErrorResponse<>(new Error(400, "No find posts"));
+        }
+        return new SuccessResponse<>(posts);
     }
 }
--- src/main/resources/static/js/main_page.js
+++ src/main/resources/static/js/main_page.js
@@ -48,7 +48,7 @@ async function onClickOpt(id) {
     $('#countPostButton').empty();
     let sizeArray = 0;
     posts.then(posts => {
-        posts.forEach(() => {
+        posts.data.forEach(() => {
             sizeArray++;
         })
     }).then(() => {
@@ -82,7 +82,7 @@ async function viewCities() {
     let sizeArray = 0;
     console.log(posts);
     posts.then(posts => {
-        posts.forEach(() => {
+        posts.data.forEach(() => {
             sizeArray++;
         })
     }).then(() => {
@@ -110,7 +110,7 @@ function addOptions() {
     $('.citiesOptions').append(select);
     let addForm = $(".typeahead").val().toLowerCase();
     cities.then(cities => {
-        cities.forEach(city => {
+        cities.data.forEach(city => {
             if (city.name.toLowerCase().includes(addForm)) {
                 let userRow = `<option onmouseover="onOptionHover()" 
                                        onclick="onClickOpt(this.id)"
