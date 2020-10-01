cb8557f6c203de902fb62ee7834774c0aa95a98e
SDA-mind <dima-sherbakov@mail.ru>
2020-10-01 20:19:08 +0300

    refactor 14

--- src/main/resources/static/js/main_page_body.js
+++ src/main/resources/static/js/main_page_body.js
@@ -14,10 +14,42 @@ function getPostingsTable() {
 
                 document.getElementById('mainPageBody').innerHTML +=
                     `<div id="main_page_posting" class="col-md-3">
-                        <div class="card">
-                            <a  href="#">
-                            <img id="postingImageRef" class="card-img-top" src="../images/logo.jpg" alt="">
-                            </a>
+                        <div id="cardPosting" class="card">
+                                <a href=""><img id="postingImageRef" class="card-img-top" src="../images/empty_image.jpg" alt=""></a>
+<!--                            <div id="ImageSlider" class="carousel slide" data-ride="carousel">-->
+<!--                                <ol class="carousel-indicators">-->
+<!--                                    <li data-target="#ImageSlider" data-slide-to="0" class="active"></li>-->
+<!--                                    <li data-target="#ImageSlider" data-slide-to="1"></li>-->
+<!--                                    <li data-target="#ImageSlider" data-slide-to="2"></li>-->
+<!--                                </ol>-->
+<!--                                <div id="carouselInner" class="carousel-inner">-->
+<!--                                    <div class="carousel-item active">-->
+<!--                                        <a href="#">-->
+<!--                                            <img id="postingImageRef" src="../images/logo.jpg" class="card-img-top" alt="">-->
+<!--                                        </a>-->
+<!--                                    </div>-->
+<!--                                    <div class="carousel-item">-->
+<!--                                        <a href="#">-->
+<!--                                            <img id="postingImageRef" src="../images/logo.jpg" class="card-img-top" alt="">-->
+<!--                                        </a>-->
+<!--                                    </div>-->
+<!--                                    <div class="carousel-item">-->
+<!--                                        <a>-->
+<!--                                            <img id="postingImageRef" src="../images/logo.jpg" class="card-img-top" alt="">-->
+<!--                                        </a>-->
+<!--                                    </div>-->
+<!--                                </div>-->
+<!--                                <a class="carousel-control-prev" href="#ImageSlider" role="button"-->
+<!--                                   data-slide="prev">-->
+<!--                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>-->
+<!--                                    <span class="sr-only">Previous</span>-->
+<!--                                </a>-->
+<!--                                <a class="carousel-control-next" href="#ImageSlider" role="button"-->
+<!--                                   data-slide="next">-->
+<!--                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>-->
+<!--                                    <span class="sr-only">Next</span>-->
+<!--                                </a>-->
+<!--                            </div>-->
                             <div id="postingCardBody" class="card-body">
                                 <a id="postingTitle" class="text-primary" href="#">${postingDTO.title}</a>
                                 <strong>
