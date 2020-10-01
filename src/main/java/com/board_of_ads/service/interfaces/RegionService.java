97cb4f024fa13708b0cdbf3de3d539f7fa8d06ca
SDA-mind <dima-sherbakov@mail.ru>
2020-10-01 09:13:17 +0300

    refactor 10

--- src/main/java/com/board_of_ads/service/interfaces/RegionService.java
+++ src/main/java/com/board_of_ads/service/interfaces/RegionService.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.Optional;
 
 public interface RegionService {
+
     Optional<Region> findRegionByNameAndFormSubject(String name);
+
     List<Region> findAll();
 }
