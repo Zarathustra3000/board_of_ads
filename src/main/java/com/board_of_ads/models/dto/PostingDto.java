ebb4ebb767debb3de29e71b6ea27eeafffea2ab0
SDA-mind <dima-sherbakov@mail.ru>
2020-10-01 12:00:36 +0300

    refactor 12

--- src/main/java/com/board_of_ads/models/dto/PostingDto.java
+++ src/main/java/com/board_of_ads/models/dto/PostingDto.java
@@ -1,14 +1,20 @@
 package com.board_of_ads.models.dto;
 
+import com.board_of_ads.models.Image;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Data
 @AllArgsConstructor
-public class PostingDto  {
+public class PostingDto {
     private Long id;
     private String title;
     private String description;
     private Long price;
     private String contact;
+    private LocalDateTime datePosting;
+//    private List<Image> images;
 }
--- src/main/resources/static/js/main_page_body.js
+++ src/main/resources/static/js/main_page_body.js
@@ -1,32 +1,36 @@
 function getPostingsTable() {
     $.ajax({
         type: "GET",
-        url: '/api/posting/list',
+        url: '/api/posting',
         dataType: 'json',
         async: true,
         success: function (data) {
-            // for (let step = 0; step < data.length; step++) {
-            //     console.log(data[step])
-            // }
-            console.log(data)
+            for (let step = 0; step < data.length; step++) {
+                let postingDTO = data[step]
+                let date = postingDTO.datePosting.substring(8,10) + "-" +
+                    postingDTO.datePosting.substring(5,7) + "-" +
+                    postingDTO.datePosting.substring(0,4) + " " +
+                    postingDTO.datePosting.substring(11,16);
+
+                document.getElementById('mainPageBody').innerHTML +=
+                    `<div class="col-md-3" style="height: 320px;width: 208px">
+                        <div class="card">
+                            <a href="#"><img class="card-img-top" src="../images/logo.jpg" alt=""
+                            style="width: 100%;height: 200px; display: block"></a>
+                                <div class="card-body">
+                                    <a href="#">${postingDTO.title}</a>
+                                    <strong>
+                                        <div id="price">${postingDTO.price} ₽</div>
+                                    </strong>
+                                    <div class="card-text text-muted">
+                                        <div id="meetingPlace">Москва, Преображенская площадь</div>
+                                        <div id="timeOfPosting">${date}</div>
+                                    </div>
+                                </div>
+                        </div>
+                    </div>`
+            }
         }
     })
-    // document.getElementById('mainPageBody').innerHTML +=
-    //     `<div class="col-md-3">
-    //         <div class="card">
-    //             <img class="card-img-top" src="..." alt="Card image cap">
-    //                 <div class="card-body">
-    //                     <a href="#">Название карточки</a>
-    //                     <strong>
-    //                         <div id="price">Цена</div>
-    //                     </strong>
-    //                     <div class="card-text text-muted">
-    //                         <div id="meetingPlace">Москва, Преображенская площадь</div>
-    //                         <div id="timeOfPosint">25 сентября 16:10</div>
-    //                     </div>
-    //                 </div>
-    //         </div>
-    //     </div>`
-
 }
 getPostingsTable()
