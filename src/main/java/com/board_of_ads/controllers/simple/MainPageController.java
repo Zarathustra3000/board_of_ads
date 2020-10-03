a2d6821559b7a4bd5959c87d452e668838594bc2
Dmitriy <fed3375546@yandex.ru>
2020-10-03 12:05:14 +0300

    add authorization in MainController

--- src/main/java/com/board_of_ads/controllers/simple/MainPageController.java
+++ src/main/java/com/board_of_ads/controllers/simple/MainPageController.java
@@ -2,15 +2,21 @@ package com.board_of_ads.controllers.simple;
 
 import com.board_of_ads.models.User;
 import com.board_of_ads.service.interfaces.AuthService;
+import com.board_of_ads.service.interfaces.AuthorizationService;
+import com.board_of_ads.service.interfaces.UserService;
 import com.board_of_ads.service.interfaces.VkService;
 import com.board_of_ads.service.interfaces.YandexService;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -20,6 +26,8 @@ public class MainPageController {
     private final YandexService yandexService;
     private final VkService vkService;
     private final AuthService authService;
+    private final AuthorizationService authorizationService;
+    private final UserService userService;
 
     @GetMapping("/")
     public String getMainPage(@AuthenticationPrincipal() User user, Model model) {
@@ -27,6 +35,21 @@ public class MainPageController {
         model.addAttribute(user != null ? user : new User());
         return "main-page";
     }
+
+
+
+    @PostMapping("/auth")
+    public ResponseEntity<User> authorization(@RequestBody User userAuth) {
+
+        if (authorizationService.isValid(userAuth)) {
+            userAuth = userService.getUserByEmail(userAuth.getEmail());
+            authorizationService.login(userAuth);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 
     @GetMapping("/admin_page")
     public String adminPage(@AuthenticationPrincipal User user, Model model) {
