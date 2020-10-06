8d48b2ee33481242f225b4b6c120d10b833625a2
mr-dbauer <bauer.phys@gmail.com>
2020-10-06 16:44:24 +0700

    add_ok.api_auth

--- src/main/java/com/board_of_ads/controllers/simple/AuthController.java
+++ src/main/java/com/board_of_ads/controllers/simple/AuthController.java
@@ -39,6 +39,7 @@ public class AuthController {
         return "redirect:/oauth2/authorization/google";
     }
 
+
     @GetMapping("/auth_vk")
     public String vkAuth(@RequestParam(value = "code", required = false) String code, Model model) {
         if (code == null) {
