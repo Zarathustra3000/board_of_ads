3b0750b1a325886949e5040c0e07a2641091b69b
SDA-mind <dima-sherbakov@mail.ru>
2020-10-07 13:09:43 +0300

    refactor 01

--- src/main/java/com/board_of_ads/models/dto/PostingDto.java
+++ src/main/java/com/board_of_ads/models/dto/PostingDto.java
@@ -1,5 +1,7 @@
 package com.board_of_ads.models.dto;
 
+import com.board_of_ads.models.Category;
+import com.board_of_ads.models.City;
 import com.board_of_ads.models.Image;
 import com.board_of_ads.service.interfaces.PostingService;
 import lombok.AllArgsConstructor;
@@ -19,6 +21,8 @@ public class PostingDto {
     private String contact;
     private LocalDateTime datePosting;
     private List<Image> images;
+    private String category;
+    private String city;
 
     public PostingDto(Long id, String title, String description, Long price, String contact, LocalDateTime datePosting) {
         this.id = id;
--- src/main/resources/static/js/main_page_body.js
+++ src/main/resources/static/js/main_page_body.js
@@ -1,21 +1,23 @@
+let selectedCategoryOption = "Любая категория";
+let allPostings;
 
-function getPostingsTable() {
-    $.ajax({
-        type: "GET",
-        url: '/api/posting',
-        dataType: 'json',
-        async: true,
-        success: function (result) {
-            let array = result.data
-            for (let step = 0; step < array.length; step++) {
+function getPostingsTable(categorySelect, posts) {
+    for (let step = 0; step < posts.length; step++) {
+        let categorySelectTemp = categorySelect;
+        let postingDTO = posts[step];
 
-                let postingDTO = array[step]
-                let date = postingDTO.datePosting.substring(8, 10) + "-" +
-                    postingDTO.datePosting.substring(5, 7) + "-" +
-                    postingDTO.datePosting.substring(0, 4) + " " +
-                    postingDTO.datePosting.substring(11, 16);
-                document.getElementById('mainPageBody').innerHTML +=
-                    `<div id="main_page_posting" class="col-md-3">
+        let date = postingDTO.datePosting.substring(8, 10) + "-" +
+            postingDTO.datePosting.substring(5, 7) + "-" +
+            postingDTO.datePosting.substring(0, 4) + " " +
+            postingDTO.datePosting.substring(11, 16);
+
+        if (categorySelectTemp === "Любая категория") {
+            categorySelectTemp = postingDTO.category;
+        }
+
+        if (postingDTO.category === categorySelectTemp) {
+            document.getElementById('mainPageBody').innerHTML +=
+                `<div id="main_page_posting" class="col-md-3">
                         <div id="cardPosting" class="card">
                             <div id="ImageSlider${step}" class="carousel slide" data-interval="false">
                                 <ol id="carouselIndicators${step}" class="carousel-indicators">
@@ -37,52 +39,101 @@ function getPostingsTable() {
                             </div>
                         </div>
                     </div>`
+            if (postingDTO.images.length > 0) {
+                for (let i = 0; i < postingDTO.images.length; i++) {
+                    let indicator = step + "indicator" + i;
+                    if (i === 0) {
+                        document.getElementById("carouselIndicators" + step).innerHTML +=
+                            `<li id="${indicator}" data-target="#ImageSlider${step}" data-slide-to="i" class="active"></li>`
 
-                if (postingDTO.images.length > 0) {
-                    for (let i = 0; i < postingDTO.images.length; i++) {
-                        let indicator = step + "indicator" + i;
-                        if (i === 0) {
-                            document.getElementById("carouselIndicators" + step).innerHTML +=
-                                `<li id="${indicator}" data-target="#ImageSlider${step}" data-slide-to="i" class="active"></li>`
-
-                            document.getElementById("carouselInner" + step).innerHTML +=
-                                `<div class="carousel-item active">
+                        document.getElementById("carouselInner" + step).innerHTML +=
+                            `<div class="carousel-item active">
                                     <a href="#">
                                         <img id="postingImageRef" src="${postingDTO.images[i].pathURL}" class="card-img-top" alt="">
                                     </a>
                                 </div>`
-                            $("#" + indicator).on("mouseover", function () {
-                                $("#" + indicator).click();
-                            });
-                        } else {
-                            document.getElementById("carouselIndicators" + step).innerHTML +=
-                                `<li id="${indicator}" data-target="#ImageSlider${step}" data-slide-to="i"></li>`
+                        $("#" + indicator).on("mouseover", function () {
+                            $("#" + indicator).click();
+                        });
+                    } else {
+                        document.getElementById("carouselIndicators" + step).innerHTML +=
+                            `<li id="${indicator}" data-target="#ImageSlider${step}" data-slide-to="i"></li>`
 
-                            document.getElementById("carouselInner" + step).innerHTML +=
-                                `<div class="carousel-item">
+                        document.getElementById("carouselInner" + step).innerHTML +=
+                            `<div class="carousel-item">
                                     <a href="#">
                                         <img id="postingImageRef" src="${postingDTO.images[i].pathURL}" class="card-img-top" alt="">
                                     </a>
                                 </div>`
-                            $("#" + indicator).on("mouseover", function () {
-                                $("#" + indicator).click();
-                            });
-                        }
+                        $("#" + indicator).on("mouseover", function () {
+                            $("#" + indicator).click();
+                        });
                     }
-
-                } else {
-                    document.getElementById("carouselInner" + step).innerHTML +=
-                        `<div class="carousel-item active">
+                }
+            } else {
+                document.getElementById("carouselInner" + step).innerHTML +=
+                    `<div class="carousel-item active">
                                     <a href="#">
                                         <img id="postingImageRef" src="../images/empty_image.jpg" class="card-img-top" alt="">
                                     </a>
                                 </div>`
-                }
             }
         }
-    })
+    }
 }
+
+async function getList() {
+    let response = await userService.findAllPostings()
+    allPostings = (await response.json()).data;
+    getPostingsTable(selectedCategoryOption, allPostings)
+}
+
 $(document).ready(function() {
-    getPostingsTable();
+    getList();
 });
+
+function getCategoryOption(categoryOption) {
+    if($("#category-select-city option:selected").val() != null) {
+        allPostings = regionPosts;
+    }
+    for (let i = 0; i < categoryOption.options.length; i++) {
+        if (categoryOption.options[i].selected) {
+            selectedCategoryOption = (categoryOption.options[i].text);
+        }
+    }
+    reinstallTable(selectedCategoryOption,allPostings);
+}
+function searchPosts() {
+    let searchPosts =[];
+    if($("#category-select-city option:selected").val() != null) {
+        allPostings = regionPosts;
+    }
+    let textInput = $("#search-main-text").val();
+    let photoOption = $("#image-select option:selected").text()
+    for (let step = 0; step < allPostings.length; step++) {
+        if(allPostings[step].title.toLowerCase().match(textInput.toLowerCase())) {
+            if(photoOption === "С фото") {
+                if(allPostings[step].images.length > 0) {
+                    searchPosts.push(allPostings[step]);
+                }
+            } else if(photoOption === "Без фото") {
+                if(allPostings[step].images.length === 0
+                    || allPostings[step].images.length === null) {
+                    searchPosts.push(allPostings[step]);
+                }
+            } else {
+                searchPosts.push(allPostings[step]);
+            }
+        }
+    }
+    reinstallTable(selectedCategoryOption,searchPosts)
+}
+
+function reinstallTable(categoryOption,citiesArrayOption) {
+    document.querySelectorAll('#main_page_posting').forEach(block => block.remove())
+
+    getPostingsTable(categoryOption,citiesArrayOption);
+}
+
+
 
