992e3364eda1cd36ce5668f19d9c92aec0e09a56
SidAilenMe <sidailen.me@gmail.com>
2020-10-07 09:49:03 +0300

    1 table done

--- src/main/resources/static/js/newpost.js
+++ src/main/resources/static/js/newpost.js
@@ -1,17 +1,62 @@
-$(document).ready(function (){
+$(document).ready(function () {
     getCategoryTable();
 })
 
+function getCategoryTable2(categoryName) {
+    $.ajax({
+        type: "GET",
+        url: "/api/category",
+        dataType: "json",
+        async: true,
+        success: function (result) {
+            let array = result.data;
 
-function clickOnCategoryButton() {
-    $(".category-table-button")
-        .click(function () {
-            $(".category-table-button").removeClass("active-category-table-button")
-                    .addClass("unactive-category-table-button").css("background-color", "#fff");
-            $(this).removeClass("unactive-category-table-button")
-                    .addClass("active-category-table-button").css("background-color", "#0af", "color", "#fff");
-    });
+            for (let i = 0; i < array.length; i++) {
+                let x = array[i];
+                if (x.parentTitle == categoryName) {
+                    document.getElementById("cascade-table-2").innerHTML +=
+                        `<div class="category-table-button unactive-category-table-button" id="category-table-button" 
+                            onclick="clickOnCategoryButton()" onmouseover="hoverOnCategoryButton()">
+                        ${x.name}
+                    </div>`
+                }
+            }
+        }
+    })
 }
+
+function getCategoryTable() {
+    $.ajax({
+        type: "GET",
+        url: '/api/category',
+        dataType: "json",
+        async: true,
+        success: function (result) {
+            let array = result.data;
+
+            for (let i = 0; i < array.length; i++) {
+                let x = array[i];
+                if (x.parent == true) {
+                    document.getElementById("cascade-table").innerHTML +=
+                        `<div class="category-table-button unactive-category-table-button" id="category-table-button" 
+                            onclick="clickOnCategoryButton(this,'${x.name}')" onmouseover="hoverOnCategoryButton()">
+                        ${x.name}
+                    </div>`
+                }
+            }
+        }
+
+    })
+}
+
+function clickOnCategoryButton(o, categoryName) {
+    $(".category-table-button").removeClass("active-category-table-button")
+        .addClass("unactive-category-table-button").css("background-color", "#fff");
+    $(o).removeClass("unactive-category-table-button")
+        .addClass("active-category-table-button").css("background-color", "#0af", "color", "#fff");
+    console.log(categoryName);
+};
+
 
 function hoverOnCategoryButton() {
     $(".category-table-button").hover(
@@ -23,32 +68,8 @@ function hoverOnCategoryButton() {
             if (!this.classList.contains("active-category-table-button")) {
                 $(this).css("background-color", "#fff");
             }
+            console.log("qwe")
         });
-}
-
-function getCategoryTable() {
-    $.ajax({
-        type: "GET",
-        url: '/api/category',
-        dataType: "json",
-        async: true,
-        success: function (result) {
-            console.log(result.data);
-            let array = result.data;
-
-            for (let i = 0; i < array.length; i++) {
-                let x = array[i];
-                if (x.parent == true) {
-                    document.getElementById("cascade-table").innerHTML +=
-                        `<div class="category-table-button unactive-category-table-button" id="category-table-button" 
-                            onclick="clickOnCategoryButton()" onmouseover="hoverOnCategoryButton()">
-                        ${x.name}
-                    </div>`
-                }
-            }
-        }
-
-    })
 }
 
 
