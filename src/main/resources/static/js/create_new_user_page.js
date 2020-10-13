ba1f8733ce11ffc1aace22f561266a94535dad28
Sanaris <karpovvasilii@gmail.com>
2020-10-13 09:57:55 +0300

    1. Доделана автоматическая авторизация и редирект после регистрации

--- src/main/resources/static/js/create_new_user_page.js
+++ src/main/resources/static/js/create_new_user_page.js
@@ -1,36 +1,73 @@
 let newUserUrl = 'http://localhost:5556/api/user/modal-reg';
 let elementCreateUser = document.getElementById('createUser');
 
+$(document).ready(function () {
+
+});
+
 elementCreateUser.onclick = function () {
-  regNewUser();
+    regNewUser();
 };
 
 async function regNewUser() {
     let data = {
 
-        firsName: $('#userName').val(),
-        password: $('#password').val(),
-        email: $('#email').val(),
+        firsName: $('#userNameReg').val(),
+        password: $('#userPasswordReg').val(),
+        email: $('#userEmailReg').val(),
 
     };
 
-    console.log(data);
+    let userFirstNameFormData = document.getElementById('userNameReg').value;
+    let userEmailFormData = document.getElementById('userEmailReg').value;
+    let userPasswordFormData = document.getElementById('userPasswordReg').value;
 
-    const response = await fetch(newUserUrl, {
-        method: 'POST',
-        mode: 'cors',
-        cache: 'no-cache',
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        redirect: 'follow',
-        referrerPolicy: 'no-referrer',
-        body: JSON.stringify(data)
-    })
-        .catch(error => {
-            console.log(error);
-        });
 
-    return response.json();
+    if (userEmailFormData !== "" &&
+        (userPasswordFormData.length >= 6 && userPasswordFormData.length <= 60
+            && userPasswordFormData.match(/\s/) === null)
+        && userFirstNameFormData !== "") {
+        const response = await fetch(newUserUrl, {
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer',
+            body: JSON.stringify(data)
+        })
+            .catch(error => {
+                console.log(error);
+            }).then(response => {
+                if (response.status === 200) {
+                    location.reload();
+                }
+            });
+        return response.json();
+    } else {
+        if (userEmailFormData === "") {
+            document.getElementById("emailErrorsNU").innerText = "Empty field";
+        } else {
+            document.getElementById("emailErrorsNU").innerText = "";
+        }
+        if (userPasswordFormData.length < 6 || userPasswordFormData.length > 60) {
+            document.getElementById("passwordErrorsNU").innerText = "Required between 6 and 60 symbols";
+        } else {
+            document.getElementById("passwordErrorsNU").innerText = "";
+        }
+        if (userPasswordFormData.match(/\s/) !== null) {
+            document.getElementById("passwordSpaceNotAllow").innerText = "Space not allowed";
+        } else {
+            document.getElementById("passwordSpaceNotAllow").innerText = "";
+        }
+        if (userFirstNameFormData === "") {
+            document.getElementById("firstnameErrorsNU").innerText = "Empty field";
+        } else {
+            document.getElementById("firstnameErrorsNU").innerText = "";
+        }
+        document.getElementById("createUserResult").innerText = "Creation failed";
+    }
 }
